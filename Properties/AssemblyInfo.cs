1c893c3b70c62f8d5417e64bbd97c482c8072686
poluv <poluv@sidharreddy123>
2021-08-31 08:49:08 +0530

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExtentReport")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExtentReport")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("37d791dd-83e3-4fd8-bf49-f65b1af59cc3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test/SearchFunction.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using NLog;
+using AventStack.ExtentReports;
+
+
+namespace ExtentReport.Test
+{
+    [TestClass]
+    public class SearchFunction
+    {
+
+       // private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        protected IWebDriver Driver { get; private set; }
+        public TestContext TestContext { get; set; }
+        private Screenshottaken Screenshottaken { get; set; }
+
+        public IWebDriver driver;
+        public string  url;
+        [TestInitialize]
+        public void intilization()
+        {
+            //  Logger.Debug("Test start");
+            Reporter.AddTestCaseMetadataToHtmlReport(TestContext);
+
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            url  = "https://www.google.com/";
+            driver.Navigate().GoToUrl(url);
+            
+            Screenshottaken = new Screenshottaken(driver, TestContext);
+
+        }
+
+        [TestMethod]
+        public void TcId1()
+        {
+            Reporter.LogTestStepForBugLogger(Status.Info, "Home page got loaded succefully");
+           
+            Assert.IsTrue(driver.Title.Contains("Google"));
+        }
+        [TestMethod]
+        public void TcId2()
+        {
+            //  Reporter.LogTestStepForBugLogger(Status.Info, "Home page got loaded succefully");
+
+            Reporter.LogPassingTestStepToBugLogger($"following url opened=>{url}");
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            var search = driver.FindElement(By.Name("q"));
+            search.SendKeys("selenium");
+            search.SendKeys(Keys.Enter);
+            Assert.IsTrue(driver.Title.Contains("Selenium google search"));
+            driver.Close();
+        }
+        [TestCleanup]
+        public void tcid3() 
+        {
+            driver.Close();
+
+        }
+
+       
+    }
+}
